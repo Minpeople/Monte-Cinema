ab10691add3471a0183fecc259f4033475bd9ac3
Minpeople <paulina.markowska@monterail.com>
2021-08-05 13:24:40 +0200

    Created recipe-hall and screening models

---
+++ db/migrate/20210804121256_create_cinema_halls.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateCinemaHalls < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cinema_halls do |t|
+      t.string "name"
+      t.integer "capacity"
+
+      t.timestamps
+    end
+  end
+end
