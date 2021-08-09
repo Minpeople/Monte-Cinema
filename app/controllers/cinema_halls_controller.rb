38248092d115f9a77c66733f769250559c0923e3
Minpeople <paulina.markowska@monterail.com>
2021-08-09 10:52:51 +0200

    Added RSpec and fixed code style

---
+++ app/controllers/cinema_halls_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class CinemaHallsController < ApplicationController
+  def index
+    @cinema_halls = CinemaHall.all
+    render json: @cinema_halls
+  end
+
+  def create
+    @cinema_hall = CinemaHall.new(cinema_hall_params)
+
+    if @cinema_hall.save
+      render json: @cinema_halls,
+             status: :created, location: @cinema_hall
+    else
+      render json: @cinema_halls.errors,
+             status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @cinema_hall = CinemaHall.find(params[:id])
+    render json: @cinema_halls
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
+  end
+
+  def update
+    if @cinema_hall.update(cinema_hall_params)
+      render json: :show, status: :ok, location: @cinema_hall
+    else
+      render json: @cinema_hall.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @cinema_hall = CinemaHall.find(params[:id])
+    @cinema_hall.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def cinema_hall_params
+    params.require(:cinema_hall).permit(:name, :capacity)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
+=======
+  resources :cinema_halls
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
