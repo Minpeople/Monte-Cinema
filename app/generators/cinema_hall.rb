12ef8fcace82c974304fe4a28456f46110ab8d48
Minpeople <paulina.markowska@monterail.com>
2021-08-09 09:45:26 +0200

    Fix code style with rubocop

---
+++ app/generators/cinema_hall.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Generators
+  class CinemaHall
+    AVAILABLE_COLUMNS = ('A'..'J')
+
+    def generate_seats(capacity)
+      seats = []
+      (1..capacity / AVAILABLE_COLUMNS.count).each do |row|
+        AVAILABLE_COLUMNS.each { |column| seats.push("#{row}#{column}") }
+      end
+
+      seats
+    end
+  end
+end
---
+++ db/migrate/20210804121256_create_cinema_halls.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateCinemaHalls < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cinema_halls do |t|
+      t.string 'name', null: false
+      t.integer 'capacity', null: false
+
+      t.timestamps
+    end
+  end
+end
