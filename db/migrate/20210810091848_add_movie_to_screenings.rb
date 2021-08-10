66d34c19f6b2e9caa71299b11b028c4847b2ec34
Minpeople <82415156+Minpeople@users.noreply.github.com>
2021-08-10 11:32:32 +0200

    Delete 20210810091848_add_movie_to_screenings.rb

--- db/migrate/20210810091848_add_movie_to_screenings.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddMovieToScreenings < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :screenings, :movie, null: false, foreign_key: true, index:true 
-  end
-end
