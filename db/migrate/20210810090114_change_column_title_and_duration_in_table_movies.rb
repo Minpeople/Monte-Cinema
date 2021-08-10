91362cf0ec1d15820135db6e4f4fb428e218869e
Minpeople <82415156+Minpeople@users.noreply.github.com>
2021-08-10 11:32:40 +0200

    Delete 20210810090114_change_column_title_and_duration_in_table_movies.rb

--- db/migrate/20210810090114_change_column_title_and_duration_in_table_movies.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeColumnTitleAndDurationInTableMovies < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null(:movies, :title, false)
-    change_column_null(:movies, :duration, false)
-  end
-end
