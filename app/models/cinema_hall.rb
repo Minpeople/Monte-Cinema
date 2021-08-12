d3fcede9b87443388a67f4dbb22f573bdd3875d3
Minpeople <82415156+Minpeople@users.noreply.github.com>
2021-08-12 12:14:02 +0200

    Update cinema_hall.rb

--- app/models/cinema_hall.rb
+++ app/models/cinema_hall.rb
@@ -2,8 +2,5 @@
 
 class CinemaHall < ApplicationRecord
   has_many :screenings, dependent: :destroy
-<<<<<<< HEAD
-=======
   has_many :seats, dependent: destroy
->>>>>>> 84f6e12 (Updated feature seats as model')
 end
