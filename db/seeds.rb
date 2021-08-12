1f33fa1b251b308eac06ede100e207515893bd76
Minpeople <paulina.markowska@monterail.com>
2021-08-12 13:31:44 +0200

    Update seeds with more cinema halls

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
-cinema_hall = CinemaHall.create([{name: 'Suzuki', capacity: 200},
-                                 { name: 'Yamata', capacity: 100},
-                                 { name: 'Katana', capacity: 50},
-                                 { name: 'Chiisai', capacity: 20}])
+cinema_hall = CinemaHall.create([{name: 'Tokyo', capacity: 200},
+                                 { name: 'Yokohama', capacity: 100},
+                                 { name: 'Osaka', capacity: 100},
+                                 { name: 'Nagoya', capacity: 100},
+                                 { name: 'Sapporo', capacity: 100},
+                                 { name: 'Kobe', capacity: 50},
+                                 { name: 'Kyoto', capacity: 50},
+                                 { name: 'Fukuoka', capacity: 50},
+                                 { name: 'Kawasaki', capacity: 50},
+                                 { name: 'Hiroshima', capacity: 20}])
 
 seats = CinemaHalls::GenerateSeats.new.generate_seats(cinema_hall.capacity)
 seats.each do |seat|
