72eb137d32cc92bb70b3d0d7e846b669cfafce26
Minpeople <paulina.markowska@monterail.com>
2021-08-12 14:07:07 +0200

    Update seeds and fix code in generate_seats

--- app/generators/cinema_halls/generate_seats.rb
+++ app/generators/cinema_halls/generate_seats.rb
@@ -4,7 +4,8 @@ module CinemaHalls
   class GenerateSeats
     AVAILABLE_COLUMNS = ('A'..'J')
 
-  def generate_seats(capacity) 
-    (1..capacity / AVAILABLE_COLUMNS.count).to_a.product(AVAILABLE_COLUMNS.to_a).map(&:join)
-  end
+    def generate_seats(capacity) 
+      (1..capacity / AVAILABLE_COLUMNS.count).to_a.product(AVAILABLE_COLUMNS.to_a).map(&:join)
+    end
+  end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,23 @@
 # frozen_string_literal: true
 
-cinema_hall = CinemaHall.create([{name: 'Tokyo', capacity: 200},
-                                 { name: 'Yokohama', capacity: 100},
-                                 { name: 'Osaka', capacity: 100},
-                                 { name: 'Nagoya', capacity: 100},
-                                 { name: 'Sapporo', capacity: 100},
-                                 { name: 'Kobe', capacity: 50},
-                                 { name: 'Kyoto', capacity: 50},
-                                 { name: 'Fukuoka', capacity: 50},
-                                 { name: 'Kawasaki', capacity: 50},
-                                 { name: 'Hiroshima', capacity: 20}])
+cinema_halls_list = [
+ { name: 'Tokyo', capacity: 200},
+ { name: 'Yokohama', capacity: 100},
+ { name: 'Osaka', capacity: 100},
+ { name: 'Nagoya', capacity: 100},
+ { name: 'Sapporo', capacity: 100},
+ { name: 'Kobe', capacity: 50},
+ { name: 'Kyoto', capacity: 50},
+ { name: 'Fukuoka', capacity: 50},
+ { name: 'Kawasaki', capacity: 50},
+ { name: 'Hiroshima', capacity: 20}
+]
+                                 
+cinema_halls_list.each do |cinema_hall_parameters|
+  cinema_hall = CinemaHall.create(cinema_hall_parameters)
+  seats = CinemaHalls::GenerateSeats.new.generate_seats(cinema_hall.capacity)
 
-seats = CinemaHalls::GenerateSeats.new.generate_seats(cinema_hall.capacity)
-seats.each do |seat|
+  seats.each do |seat|
     cinema_hall.seats.create(code: seat)
+  end
 end
