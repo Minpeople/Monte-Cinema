84f6e1287ed0ec9409e5036ddb412e345c24a3ec
Minpeople <paulina.markowska@monterail.com>
2021-08-12 12:03:36 +0200

    Updated feature seats as model'

---
+++ app/models/cinema_hall.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class CinemaHall < ApplicationRecord
+  has_many :screenings, dependent: :destroy
+  has_many :seats, dependent: destroy
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-=======
   resources :cinema_halls
->>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
