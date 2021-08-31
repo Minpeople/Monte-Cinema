603521bd19bb710efd64fe5c6f692660bd81a3e8
Minpeople <82415156+Minpeople@users.noreply.github.com>
2021-08-31 19:57:21 +0200

    Update and rename 20210831103250_change_columnin_user_table.rb to 20210831103250_change_users_role_column_default.rb

--- db/migrate/20210831103250_change_users_role_column_default.rb
+++ db/migrate/20210831103250_change_users_role_column_default.rb
@@ -1,4 +1,4 @@
-class ChangeColumninUserTable < ActiveRecord::Migration[6.1]
+class ChangeUsersRoleColumnDefault < ActiveRecord::Migration[6.1]
   def change
     change_column_default :users, :role, from: "client", to: nil
   end
