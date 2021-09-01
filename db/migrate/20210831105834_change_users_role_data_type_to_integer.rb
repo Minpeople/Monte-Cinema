6f898c17bdb785366879863b6c09cec271662ddb
Minpeople <82415156+Minpeople@users.noreply.github.com>
2021-09-01 09:33:55 +0200

    Delete 20210831105834_change_users_role_data_type_to_integer.rb

--- db/migrate/20210831105834_change_users_role_data_type_to_integer.rb
+++
@@ -1,9 +0,0 @@
-class ChangeUsersRoleDataTypeToInteger < ActiveRecord::Migration[6.1]
-  def up
-    change_column(:users, :role, :integer, using: "role::integer")
-  end
-
-  def down
-    change_column(:users, :role, :text, using: "role::text")
-  end
-end
