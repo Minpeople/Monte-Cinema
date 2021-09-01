f5f317ee9d049decf34a60d8104d967f422919c9
Minpeople <82415156+Minpeople@users.noreply.github.com>
2021-09-01 09:32:51 +0200

    Delete cinema_hall.rb

--- app/models/cinema_hall.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class CinemaHall < ApplicationRecord
-  validates_presence_of :name
-  validates_presence_of :capacity
-  has_many :screenings, dependent: :destroy
-  has_many :seats, dependent: :destroy
-end
